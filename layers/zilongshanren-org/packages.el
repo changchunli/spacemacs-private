32e039119e8affc752184c1dbe6389455986adb0
Changchun Li <changchunli93@gmail.com>
2017-08-31 15:50:17 +0800

    Add purcell's emacs org settings

--- layers/zilongshanren-org/packages.el
+++ layers/zilongshanren-org/packages.el
@@ -1,4 +1,5 @@
 ;;; packages.el --- zilong-ui layer packages file for Spacemacs.
+
 ;;
 ;; Copyright (c) 2014-2016 zilongshanren
 ;;
@@ -80,7 +81,124 @@
       (setq org-agenda-span 'day)
       (setq org-agenda-use-tag-inheritance nil) ;; 3-4x speedup
       (setq org-agenda-window-setup 'current-window)
-      (setq org-log-done t)
+      (setq org-log-done t
+            org-edit-timestamp-down-means-later t
+            org-archive-mark-done nil
+            org-hide-emphasis-markers t
+            org-catch-invisible-edits 'show
+            org-export-coding-system 'utf-8
+            org-fast-tag-selection-single-key 'expert
+            org-html-validation-link nil
+            org-export-kill-product-buffer-when-displayed t
+            org-tags-column 80
+            org-support-shift-select t)
+
+;;; from https://github.com/purcell/emacs.d/blob/master/lisp/init-org.el
+
+      ;; Lots of stuff from http://doc.norang.ca/org-mode.html
+
+      (defun sanityinc/grab-ditaa (url jar-name)
+        "Download URL and extract JAR-NAME as `org-ditaa-jar-path'."
+        ;; TODO: handle errors
+        (message "Grabbing " jar-name " for org.")
+        (let ((zip-temp (make-temp-name "emacs-ditaa")))
+          (unwind-protect
+              (progn
+                (when (executable-find "unzip")
+                  (url-copy-file url zip-temp)
+                  (shell-command (concat "unzip -p " (shell-quote-argument zip-temp)
+                                         " " (shell-quote-argument jar-name)
+                                         " > " (shell-quote-argument org-ditaa-jar-path)))))
+            (when (file-exists-p zip-temp)
+              (delete-file zip-temp)))))
+
+      (with-eval-after-load 'ob-ditaa
+        (unless (and (boundp 'org-ditaa-jar-path)
+                     (file-exists-p org-ditaa-jar-path))
+          (let ((jar-name "ditaa.jar")
+                (url "http://jaist.dl.sourceforge.net/project/ditaa/ditaa/0.9/ditaa0_9.zip"))
+            (setq org-ditaa-jar-path (expand-file-name jar-name (file-name-directory user-init-file)))
+            (unless (file-exists-p org-ditaa-jar-path)
+              (sanityinc/grab-ditaa url jar-name)))))
+
+      (define-minor-mode prose-mode
+        "Set up a buffer for prose editing.
+This enables or modifies a number of settings so that the
+experience of editing prose is a little more like that of a
+typical word processor."
+        nil " Prose" nil
+        (if prose-mode
+            (progn
+              (when (fboundp 'writeroom-mode)
+                (writeroom-mode 1))
+              (setq truncate-lines nil)
+              (setq word-wrap t)
+              (setq cursor-type 'bar)
+              (when (eq major-mode 'org)
+                (kill-local-variable 'buffer-face-mode-face))
+              (buffer-face-mode 1)
+              ;;(delete-selection-mode 1)
+              (set (make-local-variable 'blink-cursor-interval) 0.6)
+              (set (make-local-variable 'show-trailing-whitespace) nil)
+              (ignore-errors (flyspell-mode 1))
+              (visual-line-mode 1))
+          (kill-local-variable 'truncate-lines)
+          (kill-local-variable 'word-wrap)
+          (kill-local-variable 'cursor-type)
+          (kill-local-variable 'show-trailing-whitespace)
+          (buffer-face-mode -1)
+          ;; (delete-selection-mode -1)
+          (flyspell-mode -1)
+          (visual-line-mode -1)
+          (when (fboundp 'writeroom-mode)
+            (writeroom-mode 0))))
+
+      ;;(add-hook 'org-mode-hook 'buffer-face-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Refiling
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+      (setq org-refile-use-cache nil)
+
+      ;; Targets include this file and any file contributing to the agenda - up to 5 levels deep
+      (setq org-refile-targets '((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5)))
+
+      (with-eval-after-load 'org-agenda
+        (add-to-list 'org-agenda-after-show-hook 'org-show-entry))
+
+      (defadvice org-refile (after sanityinc/save-all-after-refile activate)
+        "Save all org buffers after each refile operation."
+        (org-save-all-org-buffers))
+
+      ;; Exclude DONE state tasks from refile targets
+      (defun sanityinc/verify-refile-target ()
+        "Exclude todo keywords with a done state from refile targets."
+        (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+      (setq org-refile-target-verify-function 'sanityinc/verify-refile-target)
+
+      (defun sanityinc/org-refile-anywhere (&optional goto default-buffer rfloc msg)
+        "A version of `org-refile' which allows refiling to any subtree."
+        (interactive "P")
+        (let ((org-refile-target-verify-function))
+          (org-refile goto default-buffer rfloc msg)))
+
+      (defun sanityinc/org-agenda-refile-anywhere (&optional goto rfloc no-update)
+        "A version of `org-agenda-refile' which allows refiling to any subtree."
+        (interactive "P")
+        (let ((org-refile-target-verify-function))
+          (org-agenda-refile goto rfloc no-update)))
+
+      ;; Targets start with the file name - allows creating level 1 tasks
+      ;;(setq org-refile-use-outline-path (quote file))
+      (setq org-refile-use-outline-path t)
+      (setq org-outline-path-complete-in-steps nil)
+
+      ;; Allow refile to create parent tasks with confirmation
+      (setq org-refile-allow-creating-parent-nodes 'confirm)
+
+
+
 
       ;; 加密文章
       ;; "http://coldnew.github.io/blog/2013/07/13_5b094.html"
@@ -104,11 +222,26 @@
       ;; (add-to-list 'auto-mode-alist '("\.org\\'" . org-mode))
 
       (setq org-todo-keywords
-            (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
-                    (sequence "WAITING(w@/!)" "SOMEDAY(S)" "|" "CANCELLED(c@/!)" "MEETING(m)" "PHONE(p)"))))
+            (quote ((sequence "TODO(t)" "STARTED(s)" "NEXT(n)" "|" "DONE(d!/!)")
+                    (sequence "PROJECT(p)" "|" "DONE(d!/!)" "CANCELLED(c@/!)")
+                    (sequence "WAITING(w@/!)" "SOMEDAY(S)" "DELEGATED(e!)" 
+                              "HOLD(h)" "|" "CANCELLED(c@/!)" "MEETING(m)" "PHONE(p)")))
+            org-todo-repeat-to-state "NEXT")
+
+      (setq org-todo-keyword-faces
+            (quote (("NEXT" :inherit warning)
+                    ("PROJECT" :inherit font-lock-string-face))))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
       ;; Org clock
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+      ;; Save the running clock and all clock history when exiting Emacs, load it on startup
+      (after-load 'org
+                  (org-clock-persistence-insinuate))
+      (setq org-clock-persist t)
+      (setq org-clock-in-resume t)
 
       ;; Change task state to STARTED when clocking in
       (setq org-clock-in-switch-to-state "STARTED")
@@ -116,6 +249,42 @@
       (setq org-clock-into-drawer t)
       ;; Removes clocked tasks with 0:00 duration
       (setq org-clock-out-remove-zero-time-clocks t) ;; Show the clocked-in task - if any - in the header line
+      ;; Show clock sums as hours and minutes, not "n days" etc.
+      (setq org-time-clocksum-format
+            '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+
+      ;; Remove empty LOGBOOK drawers on clock out
+      (defun sanityinc/remove-empty-drawer-on-clock-out ()
+        (interactive)
+        (save-excursion
+          (beginning-of-line 0)
+          (org-remove-empty-drawer-at "LOGBOOK" (point))))
+
+      (after-load 'org-clock
+                  (add-hook 'org-clock-out-hook 'sanityinc/remove-empty-drawer-on-clock-out 'append))
+
+
+
+      ;; TODO: warn about inconsistent items, e.g. TODO inside non-PROJECT
+      ;; TODO: nested projects!
+
+
+;;; Archiving
+
+      (setq org-archive-mark-done nil)
+      (setq org-archive-location "%s_archive::* Archive")
+
+
+;;; Show the clocked-in task - if any - in the header line
+      (defun sanityinc/show-org-clock-in-header-line ()
+        (setq-default header-line-format '((" " org-mode-line-string " "))))
+
+      (defun sanityinc/hide-org-clock-from-header-line ()
+        (setq-default header-line-format nil))
+
+      (add-hook 'org-clock-in-hook 'sanityinc/show-org-clock-in-header-line)
+      (add-hook 'org-clock-out-hook 'sanityinc/hide-org-clock-from-header-line)
+      (add-hook 'org-clock-cancel-hook 'sanityinc/hide-org-clock-from-header-line)
 
       (setq org-tags-match-list-sublevels nil)
 
@@ -199,8 +368,18 @@
       (org-babel-do-load-languages
        'org-babel-load-languages
        '((perl . t)
+         (R . t)
+         (gnuplot . t)
+         (haskell . nil)
+         (ledger . t)
+         (ocaml . nil)
+         (octave . t)
          (ruby . t)
-         (sh . t)
+         ;; (sh . t)
+         (screen . nil)
+         (,(if (locate-library "ob-sh") 'sh 'shell) . t)
+         (sql . nil)
+         (sqlite . t)
          (dot . t)
          (js . t)
          (latex .t)
@@ -254,7 +433,7 @@ unwanted space when exporting org-mode to html."
               ("s" "Code Snippet" entry
                (file org-agenda-file-code-snippet)
                "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
-              ("w" "work" entry (file+headline org-agenda-file-gtd "Cocos2D-X")
+              ("w" "work" entry (file+headline org-agenda-file-gtd "Papers")
                "* TODO [#A] %?\n  %i\n %U"
                :empty-lines 1)
               ("c" "Chrome" entry (file+headline org-agenda-file-note "Quick notes")
@@ -268,22 +447,111 @@ unwanted space when exporting org-mode to html."
                "* %?"
                :empty-lines 1)))
 
-      ;;An entry without a cookie is treated just like priority ' B '.
-      ;;So when create new task, they are default 重要且紧急
-      (setq org-agenda-custom-commands
-            '(
-              ("w" . "任务安排")
-              ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
-              ("wb" "重要且不紧急的任务" tags-todo "-Weekly-Monthly-Daily+PRIORITY=\"B\"")
-              ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
-              ("b" "Blog" tags-todo "BLOG")
-              ("p" . "项目安排")
-              ("pw" tags-todo "PROJECT+WORK+CATEGORY=\"cocos2d-x\"")
-              ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"zilongshanren\"")
-              ("W" "Weekly Review"
-               ((stuck "") ;; review stuck projects as designated by org-stuck-projects
-                (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
-                ))))
+      ;;; Agenda views
+
+      (setq-default org-agenda-clockreport-parameter-plist '(:link t :maxlevel 3))
+
+
+      (let ((active-project-match "-INBOX/PROJECT"))
+
+        (setq org-stuck-projects
+              `(,active-project-match ("NEXT")))
+
+        (setq org-agenda-compact-blocks t
+              org-agenda-sticky t
+              org-agenda-start-on-weekday nil
+              org-agenda-span 'day
+              org-agenda-include-diary nil
+              org-agenda-sorting-strategy
+              '((agenda habit-down time-up user-defined-up effort-up category-keep)
+                (todo category-up effort-up)
+                (tags category-up effort-up)
+                (search category-up))
+              org-agenda-window-setup 'current-window
+              org-agenda-custom-commands
+              `(("N" "Notes" tags "NOTE"
+                 ((org-agenda-overriding-header "Notes")
+                  (org-tags-match-list-sublevels t)))
+                ("g" "GTD"
+                 ((agenda "" nil)
+                  (tags "INBOX"
+                        ((org-agenda-overriding-header "Inbox")
+                         (org-tags-match-list-sublevels nil)))
+                  (stuck ""
+                         ((org-agenda-overriding-header "Stuck Projects")
+                          (org-agenda-tags-todo-honor-ignore-options t)
+                          (org-tags-match-list-sublevels t)
+                          (org-agenda-todo-ignore-scheduled 'future)))
+                  (tags-todo "-INBOX"
+                             ((org-agenda-overriding-header "Next Actions")
+                              (org-agenda-tags-todo-honor-ignore-options t)
+                              (org-agenda-todo-ignore-scheduled 'future)
+                              (org-agenda-skip-function
+                               '(lambda ()
+                                  (or (org-agenda-skip-subtree-if 'todo '("HOLD" "WAITING"))
+                                      (org-agenda-skip-entry-if 'nottodo '("NEXT")))))
+                              (org-tags-match-list-sublevels t)
+                              (org-agenda-sorting-strategy
+                               '(todo-state-down effort-up category-keep))))
+                  (tags-todo ,active-project-match
+                             ((org-agenda-overriding-header "Projects")
+                              (org-tags-match-list-sublevels t)
+                              (org-agenda-sorting-strategy
+                               '(category-keep))))
+                  (tags-todo "-INBOX/-NEXT"
+                             ((org-agenda-overriding-header "Orphaned Tasks")
+                              (org-agenda-tags-todo-honor-ignore-options t)
+                              (org-agenda-todo-ignore-scheduled 'future)
+                              (org-agenda-skip-function
+                               '(lambda ()
+                                  (or (org-agenda-skip-subtree-if 'todo '("PROJECT" "HOLD" "WAITING" "DELEGATED"))
+                                      (org-agenda-skip-subtree-if 'nottododo '("TODO")))))
+                              (org-tags-match-list-sublevels t)
+                              (org-agenda-sorting-strategy
+                               '(category-keep))))
+                  (tags-todo "/WAITING"
+                             ((org-agenda-overriding-header "Waiting")
+                              (org-agenda-tags-todo-honor-ignore-options t)
+                              (org-agenda-todo-ignore-scheduled 'future)
+                              (org-agenda-sorting-strategy
+                               '(category-keep))))
+                  (tags-todo "/DELEGATED"
+                             ((org-agenda-overriding-header "Delegated")
+                              (org-agenda-tags-todo-honor-ignore-options t)
+                              (org-agenda-todo-ignore-scheduled 'future)
+                              (org-agenda-sorting-strategy
+                               '(category-keep))))
+                  (tags-todo "-INBOX"
+                             ((org-agenda-overriding-header "On Hold")
+                              (org-agenda-skip-function
+                               '(lambda ()
+                                  (or (org-agenda-skip-subtree-if 'todo '("WAITING"))
+                                      (org-agenda-skip-entry-if 'nottodo '("HOLD")))))
+                              (org-tags-match-list-sublevels nil)
+                              (org-agenda-sorting-strategy
+                               '(category-keep))))
+                  ;; (tags-todo "-NEXT"
+                  ;;            ((org-agenda-overriding-header "All other TODOs")
+                  ;;             (org-match-list-sublevels t)))
+                  ))
+                ;;An entry without a cookie is treated just like priority ' B '.
+                ;;So when create new task, they are default 重要且紧急
+                ("w" . "任务安排")
+                ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
+                ("wb" "重要且不紧急的任务" tags-todo "-Weekly-Monthly-Daily+PRIORITY=\"B\"")
+                ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
+                ("b" "Blog" tags-todo "BLOG")
+                ("p" . "项目安排")
+                ("pw" tags-todo "PROJECT+WORK+CATEGORY=\"papers\"")
+                ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"changchunli\"")
+                ("W" "Weekly Review"
+                 ((stuck "") ;; review stuck projects as designated by org-stuck-projects
+                  (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
+                  )))))
+
+
+      (add-hook 'org-agenda-mode-hook 'hl-line-mode)
+
 
       (defvar zilongshanren-website-html-preamble
         "<div class='nav'>
@@ -415,9 +683,7 @@ holding contextual information."
                         ;; `org-info.js'.
                         (if (eq (org-element-type first-content) 'section) contents
                           (concat (org-html-section first-content "" info) contents))
-                        (org-html--container headline info)))))))
-
-      )))
+                        (org-html--container headline info))))))))))
 
 (defun zilongshanren-org/init-org-mac-link ()
   (use-package org-mac-link
@@ -430,7 +696,7 @@ holding contextual information."
     :defer t))
 
 (defun zilongshanren-org/post-init-ox-reveal ()
-  (setq org-reveal-root "file:////usr/william/.emacs.d/reveal-js"))
+  (setq org-reveal-root "file:////home/william/.emacs.d/reveal-js"))
 
 
 (defun zilongshanren-org/init-org-tree-slide ()
@@ -448,7 +714,7 @@ holding contextual information."
 (defun zilongshanren-org/init-plain-org-wiki ()
   (use-package plain-org-wiki
     :init
-    (setq pow-directory "~/Jot/org-notes")))
+    (setq pow-directory "~/org-notes")))
 
 (defun zilongshanren-org/init-worf ()
   (use-package worf
