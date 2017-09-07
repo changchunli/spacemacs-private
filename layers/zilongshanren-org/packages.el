381d74f557397292357a39b422806655ebc5773f
Changchun Li <changchunli93@gmail.com>
2017-09-07 10:42:09 +0800

    Add org-mode latex classes

--- layers/zilongshanren-org/packages.el
+++ layers/zilongshanren-org/packages.el
@@ -342,7 +342,74 @@ typical word processor."
       ;; To install texlive-xetex:
       ;;    `sudo USE="cjk" emerge texlive-xetex` on Gentoo Linux
       ;; }}
-      (setq org-latex-default-class "ctexart")
+
+      (add-to-list 'org-latex-classes '("my-article" "\\documentclass[10pt,a4paper]{article}
+                                        [NO-DEFAULT-PACKAGES]
+                                        \\usepackage{graphicx}
+                                        \\usepackage{xcolor}
+                                        \\usepackage{xeCJK}
+                                        \\usepackage{lmodern}
+                                        \\usepackage{verbatim}
+                                        \\usepackage{fixltx2e}
+                                        \\usepackage{longtable}
+                                        \\usepackage{float}
+                                        \\usepackage{tikz}
+                                        \\usepackage{wrapfig}
+                                        \\usepackage{soul}
+                                        \\usepackage{textcomp}
+                                        \\usepackage{listings}
+                                        \\usepackage{geometry}
+                                        \\usepackage{algorithm}
+                                        \\usepackage{algorithmic}
+                                        \\usepackage{marvosym}
+                                        \\usepackage{wasysym}
+                                        \\usepackage{latexsym}
+                                        \\usepackage{natbib}
+                                        \\usepackage{fancyhdr}
+                                        \\usepackage[xetex,colorlinks=true,CJKbookmarks=true,
+                                                      linkcolor=blue,
+                                                      urlcolor=blue,
+                                                      menucolor=blue]{hyperref}
+                                        \\usepackage{fontspec,xunicode,xltxtra}
+                                        \\setmainfont[BoldFont=Adobe Heiti Std]{Adobe Song Std}  
+                                        \\setsansfont[BoldFont=Adobe Heiti Std]{AR PL UKai CN}  
+                                        \\setmonofont{Bitstream Vera Sans Mono}  
+                                        \\newcommand\\fontnamemono{AR PL UKai CN}%等宽字体
+                                        \\newfontinstance\\MONO{\\fontnamemono}
+                                        \\newcommand{\\mono}[1]{{\\MONO #1}}
+                                        \\setCJKmainfont[Scale=0.9]{Adobe Heiti Std}%中文字体
+                                        \\setCJKmonofont[Scale=0.9]{Adobe Heiti Std}
+                                        \\hypersetup{unicode=true}
+                                        \\geometry{a4paper, textwidth=6.5in, textheight=10in,
+                                        marginparsep=7pt, marginparwidth=.6in}
+                                        \\definecolor{foreground}{RGB}{220,220,204}%浅灰
+                                        \\definecolor{background}{RGB}{62,62,62}%浅黑
+                                        \\definecolor{preprocess}{RGB}{250,187,249}%浅紫
+                                        \\definecolor{var}{RGB}{239,224,174}%浅肉色
+                                        \\definecolor{string}{RGB}{154,150,230}%浅紫色
+                                        \\definecolor{type}{RGB}{225,225,116}%浅黄
+                                        \\definecolor{function}{RGB}{140,206,211}%浅天蓝
+                                        \\definecolor{keyword}{RGB}{239,224,174}%浅肉色
+                                        \\definecolor{comment}{RGB}{180,98,4}%深褐色
+                                        \\definecolor{doc}{RGB}{175,215,175}%浅铅绿
+                                        \\definecolor{comdil}{RGB}{111,128,111}%深灰
+                                        \\definecolor{constant}{RGB}{220,162,170}%粉红
+                                        \\definecolor{buildin}{RGB}{127,159,127}%深铅绿
+                                        \\punctstyle{kaiming}
+                                        \\title{}
+                                        \\fancyfoot[C]{\\bfseries\\thepage}
+                                        \\chead{\\MakeUppercase\\sectionmark}
+                                        \\pagestyle{fancy}
+                                        \\tolerance=1000
+                                        [NO-DEFAULT-PACKAGES]
+                                        [NO-PACKAGES]"
+                                        ("\\section{%s}" . "\\section*{%s}")
+                                        ("\\subsection{%s}" . "\\subsection*{%s}")
+                                        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                                        ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                                        ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+      (setq org-latex-default-class "my-article")
       (setq org-latex-pdf-process
             '(
               "xelatex -interaction nonstopmode -output-directory %o %f"
@@ -351,6 +418,73 @@ typical word processor."
               "rm -fr %b.out %b.log %b.tex auto"))
 
       (setq org-latex-listings t)
+      ;; Options for \lset command（reference to listing Manual)
+      (setq org-latex-listings-options
+            '(
+              ("basicstyle" "\\color{foreground}\\small\\mono")           ; 源代码字体样式
+              ("keywordstyle" "\\color{function}\\bfseries\\small\\mono") ; 关键词字体样式
+              ("identifierstyle" "\\color{doc}\\small\\mono")
+              ("commentstyle" "\\color{comment}\\small\\itshape")         ; 批注样式
+              ("stringstyle" "\\color{string}\\small")                    ; 字符串样式
+              ("showstringspaces" "false")                                ; 字符串空格显示
+              ("numbers" "left")                                          ; 行号显示
+              ("numberstyle" "\\color{preprocess}")                       ; 行号样式
+              ("stepnumber" "1")                                          ; 行号递增
+              ("backgroundcolor" "\\color{background}")                   ; 代码框背景色
+              ("tabsize" "4")                                             ; TAB等效空格数
+              ("captionpos" "t")                                          ; 标题位置 top or buttom(t|b)
+              ("breaklines" "true")                                       ; 自动断行
+              ("breakatwhitespace" "true")                                ; 只在空格分行
+              ("showspaces" "false")                                      ; 显示空格
+              ("columns" "flexible")                                      ; 列样式
+              ("frame" "single")                                          ; 代码框：阴影盒
+              ("frameround" "tttt")                                       ; 代码框： 圆角
+              ("framesep" "0pt")
+              ("framerule" "8pt")
+              ("rulecolor" "\\color{background}")
+              ("fillcolor" "\\color{white}")
+              ("rulesepcolor" "\\color{comdil}")
+              ("framexleftmargin" "10mm")
+              ))
+
+      ;; 导出Beamer的设置
+      ;; allow for export=>beamer by placing #+LaTeX_CLASS: beamer in org files
+      ;;-----------------------------------------------------------------------------
+      (add-to-list 'org-export-latex-classes
+                   ;; beamer class, for presentations
+                   '("beamer"
+                              "\\documentclass[11pt,professionalfonts]{beamer}
+                              \\mode
+                              \\usetheme{{{{Warsaw}}}}
+                              %\\usecolortheme{{{{beamercolortheme}}}}
+  
+                              \\beamertemplateballitem
+                              \\setbeameroption{show notes}
+                              \\usepackage{graphicx}
+                              \\usepackage{tikz}
+                              \\usepackage{xcolor}
+                              \\usepackage{xeCJK}
+                              \\usepackage{amsmath}
+                              \\usepackage{lmodern}
+                              \\usepackage{fontspec,xunicode,xltxtra}
+                              \\usepackage{polyglossia}
+                              \\setmainfont{Times New Roman}
+                              \\setCJKmainfont{DejaVu Sans YuanTi}
+                              \\setCJKmonofont{DejaVu Sans YuanTi Mono}
+                              \\usepackage{verbatim}
+                              \\usepackage{listings}
+                              \\institute{{{{beamerinstitute}}}}
+                              \\subject{{{{beamersubject}}}}"
+                    ("\\section{%s}" . "\\section*{%s}")
+                    ("\\begin{frame}[fragile]\\frametitle{%s}"
+                     "\\end{frame}"
+                     "\\begin{frame}[fragile]\\frametitle{%s}"
+                     "\\end{frame}")))
+
+      (setq ps-paper-type 'a4
+            ps-font-size 16.0
+            ps-print-header nil
+            ps-landscape-mode nil)
 
       ;;reset subtask
       (setq org-default-properties (cons "RESET_SUBTASKS" org-default-properties))
@@ -381,6 +515,7 @@ typical word processor."
          (latex .t)
          (python . t)
          (emacs-lisp . t)
+         (matlab . t)
          (plantuml . t)
          (C . t)
          (ditaa . t)))
